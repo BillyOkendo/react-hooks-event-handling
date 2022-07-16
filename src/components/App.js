3b8d34a21697d632c281827c8acccd2478e263d6
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-16 15:17:54 +0300

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,19 @@
 import React from "react";
-import Tickler from "./Tickler";
-import MultiButton from "./MultiButton";
-import ChangeItUp from "./ChangeItUp";
-import Login from "./Login";
+// import Tickler from "./Tickler";
+// import MultiButton from "./MultiButton";
+// import ChangeItUp from "./ChangeItUp";
+// import Login from "./Login";
+
+
+function Clickable() {
+  function handleClick() {
+    console.log("click");
+  }
+  return <button onClick={handleClick}>Click Me</button>;
+}
 
 function App() {
-  return (
-    <div>
-      <h3>onClick</h3>
-      <Tickler />
-      <hr />
-
-      <MultiButton />
-      <hr />
-
-      <h3>onChange</h3>
-      <ChangeItUp />
-      <hr />
-
-      <h3>onSubmit</h3>
-      <Login />
-      <hr />
-    </div>
-  );
+  return <Clickable />;
 }
 
 export default App;
